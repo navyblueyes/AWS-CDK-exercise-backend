3b1d4b92beee8c0fd83a28da187cb1de553641df
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-08-30 22:48:44 -0500

    DynamoDB; CRUD lambdas done

--- infrastructure/GenericTable.ts
+++ infrastructure/GenericTable.ts
@@ -37,6 +37,7 @@ export class GenericTable {
   private initialize() {
     this.createTable();
     this.createLambdas();
+    this.grantTableRights();
   }
   private createTable() {
     this.table = new Table(this.stack, this.props.tableName, {
@@ -51,15 +52,34 @@ export class GenericTable {
   private createLambdas() {
     if (this.props.createLambdaPath) {
       this.createLambda = this.createSingleLambda(this.props.createLambdaPath)
+      this.createLambdaIntegration = new LambdaIntegration(this.createLambda);
     }
     if (this.props.readLambdaPath) {
       this.readLambda = this.createSingleLambda(this.props.readLambdaPath)
+      this.createLambdaIntegration = new LambdaIntegration(this.readLambda);
     }
     if (this.props.updateLambdaPath) {
       this.updateLambda = this.createSingleLambda(this.props.updateLambdaPath)
+      this.createLambdaIntegration = new LambdaIntegration(this.updateLambda);
     }
     if (this.props.deleteLambdaPath) {
       this.deleteLambda = this.createSingleLambda(this.props.deleteLambdaPath)
+      this.createLambdaIntegration = new LambdaIntegration(this.deleteLambda);
+    }
+  }
+
+  private grantTableRights() {
+    if (this.createLambda) {
+      this.table.grantWriteData(this.createLambda);
+    }
+    if (this.readLambda) {
+      this.table.grantReadData(this.readLambda);
+    }
+    if (this.updateLambda) {
+      this.table.grantWriteData(this.updateLambda);
+    }
+    if (this.deleteLambda) {
+      this.table.grantWriteData(this.deleteLambda);
     }
   }
 
@@ -69,11 +89,10 @@ export class GenericTable {
       entry: (join(__dirname, '..', 'services', this.props.tableName, `${lambdaName}.ts`)),
       handler: 'handler',
       functionName: lambdaId,
+      environment: {
+        TABLE_NAME: this.props.tableName,
+        PRIMARY_KEY: this.props.primaryKey
+      }
     })
   }
-}
-
-environment: {
-  TABLE_NAME: this.props.tableName,
-  PRIMARY_KEY: this.props.primaryKey
 }
